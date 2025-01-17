3601b8ea5b4595b668f68d83c32efd90645911e8
Samuele Converso <samu.converso@gmail.com>
2025-01-17 10:55:20 +0000

    Jan 17, 2025, 11:55 AM

--- meteo-app/src/components/Favourites.jsx
+++ meteo-app/src/components/Favourites.jsx
@@ -37,7 +37,7 @@ function Favourites() {
             <Card.Img variant="top" src="holder.js/100px180" />
             <Card.Body>
               <Card.Title>{mainCity.name}</Card.Title>
-              {/* <Card.Text>{mainCity.weather[0].description}</Card.Text> */}
+              <Card.Text>{mainCity.weather.0.description}</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
