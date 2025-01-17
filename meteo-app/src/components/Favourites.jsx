b37b1a20150a5208478a9d6cdfe35f85d08bff2d
Samuele Converso <samu.converso@gmail.com>
2025-01-17 10:32:37 +0000

    Jan 17, 2025, 11:32 AM

--- meteo-app/src/components/Favourites.jsx
+++ meteo-app/src/components/Favourites.jsx
@@ -30,17 +30,23 @@ function Favourites() {
   }, []);
 
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>{mainCity.name}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+    <Container>
+      <Row className="justify-content-center">
+        <Col>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>{mainCity.name}</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
