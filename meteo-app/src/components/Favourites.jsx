2ac3ef61c900ed7209b66cfda475f66c2ead9139
Samuele Converso <samu.converso@gmail.com>
2025-01-17 12:13:53 +0000

    Jan 17, 2025, 1:13 PM

--- meteo-app/src/components/Favourites.jsx
+++ meteo-app/src/components/Favourites.jsx
@@ -16,7 +16,7 @@ function Favourites() {
       if (response.ok) {
         let data = await response.json();
         myFavCities.push(data);
-        setMainCity(data);
+        setMainCity(mainCity.push(data));
         console.log(data);
       } else {
         throw new Error("ERROR");
