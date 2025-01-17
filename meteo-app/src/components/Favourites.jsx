4f8023023e77fd65fafd785d34a5e198a3571ad0
Samuele Converso <samu.converso@gmail.com>
2025-01-17 10:08:10 +0000

    Jan 17, 2025, 11:08 AM

--- meteo-app/src/components/Favourites.jsx
+++ meteo-app/src/components/Favourites.jsx
@@ -1,15 +1,32 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
+import { useState, useEffect } from "react";
+
 function Favourites() {
-  const URL =
-    "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}";
+  const [mainCity, setMainCity] = useState({});
+
+  const APIKEY = "e01c44c33470e1da46fde3fc07e02bee";
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=Roma,IT&appid=${APIKEY}`;
 
   const getMainCity = async function () {
     try {
       let response = await fetch(URL);
+      if (response.ok) {
+        let data = await response.json();
+        setMainCity(data);
+        console.log(data);
+      } else {
+        throw new Error("ERROR");
+      }
     } catch (error) {
       console.log(error);
     }
   };
+
+  useEffect(() => {
+    getMainCity();
+  }, []);
+
   return;
 }
 
