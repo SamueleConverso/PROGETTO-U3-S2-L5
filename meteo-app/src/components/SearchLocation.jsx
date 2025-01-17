fe35e7a78e09ec0a40c58afdac055283e692991f
Samuele Converso <samu.converso@gmail.com>
2025-01-17 14:28:34 +0000

    Jan 17, 2025, 3:28 PM

--- meteo-app/src/components/SearchLocation.jsx
+++ meteo-app/src/components/SearchLocation.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+import { Form } from "react-bootstrap";
+
 function SearchLocation() {
   return (
     <>
-      <h1>CIAO</h1>
+      <h1 className="text-center mt-5">CERCA LOCALITA'</h1>
+
+      <Form.Control type="email" placeholder="Cerca una località..." />
     </>
   );
 }
