18afbad97c0c50dbfc0b05733e408146ea3806a0
Samuele Converso <samu.converso@gmail.com>
2025-01-17 10:41:20 +0100

    .

--- meteo-app/src/App.jsx
+++ meteo-app/src/App.jsx
@@ -1,7 +1,13 @@
 //import "./App.css";
-
+import MyNavbar from "./components/MyNavbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 function App() {
-  return;
+  return (
+    <>
+      <MyNavbar />
+    </>
+  );
 }
 
 export default App;
