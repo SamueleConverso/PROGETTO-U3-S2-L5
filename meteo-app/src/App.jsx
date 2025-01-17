753276ced8e6d56b3c1cb91330a77cfab5697daa
Samuele Converso <samu.converso@gmail.com>
2025-01-17 12:46:03 +0000

    Jan 17, 2025, 1:46 PM

--- meteo-app/src/App.jsx
+++ meteo-app/src/App.jsx
@@ -3,12 +3,17 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import MyNavbar from "./components/MyNavbar";
 import Favourites from "./components/Favourites";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <MyNavbar />
-      <Favourites />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Favourites />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
