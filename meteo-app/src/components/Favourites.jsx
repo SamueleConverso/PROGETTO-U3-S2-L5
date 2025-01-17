125595bde313648f4fb244be5851c808b2ba603c
Samuele Converso <samu.converso@gmail.com>
2025-01-17 09:54:33 +0000

    Jan 17, 2025, 10:54 AM

--- meteo-app/src/components/Favourites.jsx
+++ meteo-app/src/components/Favourites.jsx
@@ -1,4 +1,15 @@
+/* eslint-disable no-unused-vars */
 function Favourites() {
+  const URL =
+    "https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&appid={API key}";
+
+  const getMainCity = async function () {
+    try {
+      let response = await fetch(URL);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return;
 }
 
