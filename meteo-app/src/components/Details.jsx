95655124a27c17a18611682c6098a31708989705
Samuele Converso <samu.converso@gmail.com>
2025-01-17 13:01:03 +0000

    Jan 17, 2025, 2:01 PM

--- meteo-app/src/components/Details.jsx
+++ meteo-app/src/components/Details.jsx
@@ -1,7 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Details() {
   const params = useParams();
+  const [city, setCity] = useState();
+  const APIKEY = "e01c44c33470e1da46fde3fc07e02bee";
+
+  const getCity = async function () {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${params.cityId},IT&appid=${APIKEY}&lang=it&units=metric`;
+    try {
+      let response = await fetch(URL);
+      if (response.ok) {
+        let data = await response.json();
+        setCity(data);
+        console.log(data);
+      } else {
+        throw new Error("ERROR");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCity();
+  }, []);
   return (
     <>
       <h1 className="text-center">Dettagli meteo</h1>
