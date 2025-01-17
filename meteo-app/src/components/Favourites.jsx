8193385ebd788d040f6b4ed482a7fba87810e696
Samuele Converso <samu.converso@gmail.com>
2025-01-17 10:29:38 +0000

    Jan 17, 2025, 11:29 AM

--- meteo-app/src/components/Favourites.jsx
+++ meteo-app/src/components/Favourites.jsx
@@ -33,7 +33,7 @@ function Favourites() {
     <Card style={{ width: "18rem" }}>
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{mainCity.name}</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
